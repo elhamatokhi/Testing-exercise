d41901a3d884817aa896f901d76994a3c0c13e41
elhamatokhi <tokhielhama30@gmail.com>
2023-01-26 12:29:13 +0430

    solved first three tests;add functions to find string length, reverse string, and create a class for calculator

--- modules/sum.js
+++ modules/sum.js
@@ -1,4 +1,49 @@
-function sum(a, b) {
-    return a + b;
+// Test 1
+
+const stringLength = (string) => {
+  const length = string.length;
+
+  if (length < 1 || length > 10) {
+    throw new Error("The number is not in range");
   }
-  module.exports = sum;
+  return length;
+};
+
+//Test 2  ---
+
+const reverseString = (string) => {
+  return string.split("").reverse().join("");
+};
+
+// Test 3
+
+class Calc {
+  constructor(n1, n2) {
+    this.n1 = n1;
+    this.n2 = n2;
+  }
+
+  subtract() {
+    return this.n1 - this.n2;
+  }
+
+  add() {
+    return this.n1 + this.n2;
+  }
+
+  multiply() {
+    return this.n1 * this.n2;
+  }
+
+  divide() {
+    return this.n1 / this.n2;
+  }
+  power() {
+    return Math.pow(this.n1, this.n2);
+  }
+}
+
+exports.reverseString = reverseString;
+exports.stringLength = stringLength;
+exports.Calc = Calc;
+
--- modules/sum.test.js
+++ modules/sum.test.js
@@ -1,5 +1,51 @@
-const sum = require('./sum');
+// const { describe } = require("@jest/globals");
+const  { stringLength,  Calc ,reverseString}   = require("./sum.js");
+// const  = require("./sum.js");
+// const Calc = require("./sum.js");
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test("string is >= 1 and <= 10", () => {
+  const name = "Elhama";
+  const lastName = "tokhi";
+  expect(stringLength(name)).toBeGreaterThan(1);
+  expect(stringLength(lastName)).toBeLessThanOrEqual(10);
+});
+
+test("This is a reversed string", () => {
+  const name = "Elhama";
+  const reversedName = "amahlE";
+
+  expect(reverseString(name)).toBe(reversedName);
+});
+
+
+describe("Calculator tester", () => {
+  const n1 = 19;
+  const n2 = 20;
+
+  const calc = new Calc(n1, n2);
+
+  test("Test addition", () => {
+    const addedNums = n1 + n2;
+    expect(calc.add()).toBe(addedNums);
+  });
+
+  test("Test subtraction", () => {
+    const subNums = n1 - n2;
+    expect(calc.subtract()).toBe(subNums);
+  });
+
+  test("Test multiplication", () => {
+    const multiplyNums = n1 * n2;
+    expect(calc.multiply()).toBe(multiplyNums);
+  });
+
+  test("Test subtraction", () => {
+    const divideNums = n1 / n2;
+    expect(calc.divide()).toBe(divideNums);
+  });
+
+  test("Test power", () => {
+    const powerNums = Math.pow(n1, n2);
+    expect(calc.power()).toBe(powerNums);
+  });
 });
